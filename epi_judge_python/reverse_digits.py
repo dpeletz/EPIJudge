c805060e3325790f8c8275d21bcb14127ef2c3b3
David Peletz <dpeletz@macalester.edu>
2020-06-03 10:09:08 -0700

    complete problem 4.8

--- epi_judge_python/reverse_digits.py
+++ epi_judge_python/reverse_digits.py
@@ -2,11 +2,42 @@ from test_framework import generic_test
 
 
 def reverse(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    # MY VERSION:
+
+    # mult, r_val, max_place, values = 1, 0, 1, []
+    #
+    # if x == 0: return x
+    # if x < 0: mult, x = -mult, -x
+    #
+    # while max_place <= x:
+    #     max_place *= 10
+    #
+    # max_place /= 10
+    # head = max_place
+    #
+    # while max_place >= 1:
+    #     values.append(x // max_place)
+    #     x, max_place = x - ((x // max_place) * max_place), max_place / 10
+    #
+    # for i in values[::-1]:
+    #     r_val += (head * i)
+    #     head /= 10
+    #
+    # return int(mult * r_val)
+
+    # CONCISE VERSION:
+    result, x_remaining = 0, abs(x)
+
+    while x_remaining:
+        # multiplies result my 10 and adds modulo of x_remaining and 10
+        result = result * 10 + (x_remaining % 10)
+        # integer divides x_remaining by 10
+        x_remaining //= 10
+    return -result if x < 0 else result
 
 
 if __name__ == '__main__':
+    # print(reverse(15))
     exit(
         generic_test.generic_test_main('reverse_digits.py',
                                        'reverse_digits.tsv', reverse))
